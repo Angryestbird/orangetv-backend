f5334c9d0ae3b74a255c25582ef185ca7be0ac7b
Qi <angryestbird@sina.com>
2020-01-02 19:21:21 +0800

    init project

---
+++ src/test/kotlin/com/orangetv/searchserver/SearchServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.orangetv.searchserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SearchServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
