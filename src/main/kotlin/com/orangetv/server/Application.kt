03b18a43e982cc74d7ca005b4286d784d59368d6
Qi <angryestbird@sina.com>
2020-02-02 16:47:40 +0800

    fulfill oauth2 code grant type

--- src/main/kotlin/com/orangetv/server/Application.kt
+++ src/main/kotlin/com/orangetv/server/Application.kt
@@ -1,31 +1,43 @@
 package com.orangetv.server
 
-import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.oauth2.OAuth2ClientProperties
+import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.core.annotation.Order
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.core.authority.AuthorityUtils
 import org.springframework.security.crypto.password.NoOpPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer
-
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer
+import org.springframework.security.oauth2.provider.client.BaseClientDetails
+import java.util.*
 
 @SpringBootApplication
 @EnableAuthorizationServer
+@EnableResourceServer
 @EnableZuulProxy
-class Application
+class Application {
+    @Bean
+    fun httpTraceRepository(): HttpTraceRepository {
+        return InMemoryHttpTraceRepository()
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<Application>(*args)
 }
 
 @Configuration
-@EnableWebSecurity
+@Order(-20)
 class SecurityConfig(
         val authenticationConfiguration: AuthenticationConfiguration
 ) : WebSecurityConfigurerAdapter() {
@@ -35,11 +47,31 @@ class SecurityConfig(
         return NoOpPasswordEncoder.getInstance()
     }
 
-//    @Autowired
-//    fun configureGlobal(auth: AuthenticationManagerBuilder) {
-//        auth.inMemoryAuthentication()
-//                .withUser("user").password("password").roles("USER")
-//                .and()
-//                .withUser("admin").password("password").roles("USER", "ADMIN")
-//    }
+    override fun configure(http: HttpSecurity) {
+        http
+                .formLogin().permitAll()
+                .and()
+                .requestMatchers().antMatchers("/login", "/oauth/authorize", "/oauth/confirm_access")
+                .and()
+                .authorizeRequests().anyRequest().authenticated()
+    }
+}
+
+@Configuration
+class BaseClientDetailsConfiguration protected constructor(private val client: OAuth2ClientProperties) {
+    @Bean
+    @ConfigurationProperties(prefix = "security.oauth2.client")
+    fun oauth2ClientDetails(): BaseClientDetails {
+        val details = BaseClientDetails()
+        if (client.clientId == null) {
+            client.clientId = UUID.randomUUID().toString()
+        }
+        details.clientId = client.clientId
+        details.clientSecret = client.clientSecret
+        details.setAuthorizedGrantTypes(Arrays.asList("authorization_code",
+                "password", "client_credentials", "implicit", "refresh_token"))
+        details.authorities = AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER")
+        details.registeredRedirectUri = setOf("http://example.com")
+        return details
+    }
 }
